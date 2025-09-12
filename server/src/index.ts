614a8d1d5897cd07013160e8d528cc1d3f43be01
Roman <kovachroman0704@gmail.com>
2025-09-12 18:37:55 +0300

    Add error handling middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import superheroRoutes from "./routes/superheroes";
+import { errorHandler } from './middlewares/errorHandler';
+
 
 dotenv.config();
 
@@ -11,11 +13,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(errorHandler);
 
 app.use("/superheroes", superheroRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/superheroes")
+  .connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/api/superheroes")
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
